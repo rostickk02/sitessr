b1443d1afa1c441f91fac6465d1b99e5e40469c4
rostickk02 <rostickk02@gmail.com>
2026-09-21 17:06:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
